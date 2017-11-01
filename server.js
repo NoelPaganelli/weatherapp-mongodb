56c1c33aee3167eb0c21a02f5853fec8dc39ce72
Noel Paganelli <noel@lacapsule.academy>
2017-11-01 10:48:00 +0100

    update with datetime

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/add', function (req, res) {
 
   request("http://api.openweathermap.org/data/2.5/weather?q="+req.query.city+"&appid=9b754f1f40051783e4f72c176953866e&units=metric&lang=fr", function(error, response, body) {
      body = JSON.parse(body);
-     var city = {name: body.name, desc: body.weather[0].description, icon: "http://openweathermap.org/img/w/"+body.weather[0].icon+".png", temp_min: body.main.temp_min+"°C", temp_max: body.main.temp_max+"°C"};
+     var city = {timestamp: Date.now(), name: body.name, desc: body.weather[0].description, icon: "http://openweathermap.org/img/w/"+body.weather[0].icon+".png", temp_min: body.main.temp_min+"°C", temp_max: body.main.temp_max+"°C"};
      cityList.push(city);
      console.log(body);
      res.render('index', {cityList});
@@ -33,7 +33,7 @@ app.get('/update', function (req, res) {
   var cityListTmp = [];
   for(var i=0; i<sortList.length; i++) {
     for(var j=0; j<cityList.length; j++){
-      if(sortList[i] == cityList[j].name){
+      if(sortList[i] == cityList[j].timestamp){
         cityListTmp.push(cityList[j]);
       }
     }
